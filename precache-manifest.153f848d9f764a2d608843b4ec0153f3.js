7ca738f666d02fcd3967f7b8917dad0616f161dd
kkeles <kkelesh@gmail.com>
2022-04-22 23:51:20 +0200

    Add files via upload

---
+++ precache-manifest.153f848d9f764a2d608843b4ec0153f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "806fd305ca8a6223cdb1",
+    "url": "/static/css/main.c8a59eba.chunk.css"
+  },
+  {
+    "revision": "806fd305ca8a6223cdb1",
+    "url": "/static/js/main.806fd305.chunk.js"
+  },
+  {
+    "revision": "98ced97ab6a34d50a4f2",
+    "url": "/static/css/1.20d82b90.chunk.css"
+  },
+  {
+    "revision": "98ced97ab6a34d50a4f2",
+    "url": "/static/js/1.98ced97a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af7f41310a123b5735b4d6927156db11",
+    "url": "/index.html"
+  }
+];
